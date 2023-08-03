2b786f593c3435b8f59487cb135ab374e4a9e84f
JonasDaJones <johnny.jonas@gmx.net>
2023-08-03 17:51:57 +0200

    basic styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Navbar } from "@/components/navbar/Navbar@";
+import { Footer } from "@/components/footer/Footer@";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navbar />
       <h1>Nun denn</h1>
+      <Footer />
     </main>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+import Logo from "../logo/Logo";
+
+const links = [
+  {
+    id: 1,
+    title: "home",
+    url: "/",
+  },
+  { id: 2, title: "overview", url: "/overview" },
+  { id: 3, title: "blog", url: "/blog" },
+  { id: 4, title: "about", url: "/about" },
+  { id: 5, title: "contact", url: "/contact" },
+  { id: 6, title: "dashboard", url: "/dashboard" },
+];
+
+export const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <Logo />
+      </Link>
+
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        <button className={styles.logout}>log out</button>
+      </div>
+    </div>
+  );
+};
